d532bcd28630969ee1aff19162ee26208fd8ddb6
AndreiCostinOlaru <125856221+AndreiCostinOlaru@users.noreply.github.com>
2025-02-18 12:01:44 +0200

    Created DataFetcher service

---
+++ Services/DataFetcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileFreaks_Coding_Challenge.Services
+{
+    public static class DataFetcher
+    {
+        public static async Task<string> FetchDataAsync(string url)
+        {
+            var httpClient = new HttpClient();
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}
