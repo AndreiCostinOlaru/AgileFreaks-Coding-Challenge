c235c5f5c30e8ef6813d6f8cb9e13f40490c7c49
AndreiCostinOlaru <125856221+AndreiCostinOlaru@users.noreply.github.com>
2025-02-18 15:31:43 +0200

    Unit test for FindClosestShops

---
+++ CoffeeShopFinderTest/UnitTest3.cs
@@ -0,0 +1,35 @@
+﻿using AgileFreaks_Coding_Challenge.Models;
+using AgileFreaks_Coding_Challenge.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace CoffeeShopFinderTest
+{
+    [TestClass]
+    public class FindClosestShopsTest
+    {
+        [TestMethod]
+        public void FindClosestShops_ReturnsCorrectOrder()
+        {
+            //Arrange
+            var userX = 0;
+            var userY = 0;
+            var shops = new List<CoffeeShop>
+            {
+                new CoffeeShop("Shop1", 1, 1),
+                new CoffeeShop("Shop2", 2, 2),
+                new CoffeeShop("Shop3", 3, 3),
+                new CoffeeShop("Shop4", 4, 4)
+            };
+
+            //Act
+            var result = FindClosestCoffeeShops.FindClosestShops(shops, userX, userY);
+
+            //Assert
+            Assert.AreEqual("Shop1", result[0].name);
+            Assert.AreEqual("Shop2", result[1].name);
+            Assert.AreEqual("Shop3", result[2].name);
+        }
+    }
+}
