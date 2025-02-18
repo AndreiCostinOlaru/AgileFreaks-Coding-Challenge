81689dde90618764e8125cb22ab9cc9bee388bc6
AndreiCostinOlaru <125856221+AndreiCostinOlaru@users.noreply.github.com>
2025-02-18 11:57:40 +0200

    Created CoffeeShop model

---
+++ Models/CoffeeShop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileFreaks_Coding_Challenge.Models
+{
+    public class CoffeeShop
+    {
+        public string Name { get; }
+        public double X { get; }
+        public double Y { get; }
+
+        public CoffeeShop(string name, double x, double y)
+        {
+            Name = name;
+            X = x;
+            Y = y;
+        }
+    }
+}
