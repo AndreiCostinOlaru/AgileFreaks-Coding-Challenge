465254fd8b42d19996873a6a1d9da347ab83c7e6
AndreiCostinOlaru <125856221+AndreiCostinOlaru@users.noreply.github.com>
2025-02-18 13:01:33 +0200

    Created FindClosestCoffeeShops service

---
+++ Services/FindClosestCoffeeShops.cs
@@ -0,0 +1,21 @@
+﻿using AgileFreaks_Coding_Challenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileFreaks_Coding_Challenge.Services
+{
+    public static class FindClosestCoffeeShops
+    {
+        public static List<(string name, double distance)> FindClosestShops(List<CoffeeShop> shops, double userX, double userY, int count = 3)
+        {
+            return shops
+                .Select(shop => (shop.Name, distance: DistanceCalculator.Calculate(userX, userY, shop.X, shop.Y)))
+                .OrderBy(shop => shop.distance)
+                .Take(count)
+                .ToList();
+        }
+    }
+}
