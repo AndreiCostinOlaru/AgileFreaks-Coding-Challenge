033b3e81645caaf638664cf4270b2b03a699fc75
AndreiCostinOlaru <125856221+AndreiCostinOlaru@users.noreply.github.com>
2025-02-18 15:13:26 +0200

    Unit Test for DataFetcher

---
+++ CoffeeShopFinderTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CoffeeShopFinderTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoffeeShopFinderTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c52adec8-53dc-498d-aea6-a96a7de3fceb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CoffeeShopFinderTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using AgileFreaks_Coding_Challenge.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CoffeeShopFinderTest
+{
+    [TestClass]
+    public class DataFetcherTests
+    {
+        [TestMethod]
+        public async void FetchDataAsync_ReturnsData()
+        {
+            //Arrange
+            var url = "https://raw.githubusercontent.com/Agilefreaks/test_oop/master/coffee_shops.csv";
+
+            //Act
+            var result = await DataFetcher.FetchDataAsync(url);
+
+            //Assert
+            Assert.IsNotNull(result.ToString());
+            Assert.IsTrue(result.ToString().Length > 0);
+        }   
+    }
+}
