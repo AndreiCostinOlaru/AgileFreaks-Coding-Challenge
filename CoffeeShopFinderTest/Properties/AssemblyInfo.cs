88665a8bd3428fd2837590459702637421230fee
AndreiCostinOlaru <125856221+AndreiCostinOlaru@users.noreply.github.com>
2025-02-19 09:02:02 +0200

    Track only essential files

--- CoffeeShopFinderTest/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("c52adec8-53dc-498d-aea6-a96a7de3fceb")]
